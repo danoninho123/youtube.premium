021c3169c345f2e8bb66b680d7f26a56175ee42e
danoninho123 <130401691+danoninho123@users.noreply.github.com>
2023-11-22 16:04:30 -0300

    .

--- youtube.premiu-main/youtubeprimeu/youtube.py
+++ youtube.premiu-main/youtubeprimeu/youtube.py
@@ -1,18 +1,24 @@
 from tkinter import *
 import os
-
-pastaApp = os.path.dirname(__file__)
-
-
+image=os.path.dirname(__file__) 
 class youtube():
     def __init__(self, master=None):
+        self.fontePadrao=('Arial','10')
         self.widget1 = Frame(master)
+        self.widget2 = Frame(master)
         self.widget1.pack()
         self.msg = Label(self.widget1, text="YOUTUBE PREMIUM")
-        self.msg.grid(column=1, row= 0)
-        self.logo=PhotoImage(file=pastaApp+'w = tk.Label(root, image=imagem)')
-        self.labelLogo = Label(self.widget1, image=self.logo)
-        self.labelLogo.grid(column=0,row=0)
+        self.msg.grid(column=1 ,row=3)
+        self.msg.pack()
+
+        self.pesquisa=Entry(self.widget1)
+        self.pesquisa["width"] = 30
+        self.pesquisa["font"] = self.fontePadrao
+        self.pesquisa.pack(side=LEFT)
+
+        self.buttonpesquisa= Button(self.buttonpesquisa)
+        
+
 root=Tk()
 youtube(root)
 root.mainloop()
